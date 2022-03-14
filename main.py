f9e442f2c50c89ce747b67beb6c123deb510717f
Ali İmran KAZAN <4414imran@gmail.com>
2022-03-14 22:48:42 +0300

    up

---
+++ main.py
@@ -0,0 +1,14 @@
+deneme = "DENEMEDENEME"
+
+print("deneme")
+print(deneme)
+print(len(deneme))
+print(len("deneme")
+print(deneme[1])
+print(deneme[3])
+print(deneme[5])
+print(deneme[0])
+print(deneme[-1])
+# print(deneme[20]) hata veriyor
+print(deneme[3:])
+print(deneme[:5])
